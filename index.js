4fb719cf3a2c133a52eca911680fbd1ee25347ea
Kyosuke Inoue <kyoro@hakamastyle.net>
2020-05-18 19:33:02 -0700

    delay message

--- index.js
+++ index.js
@@ -13,14 +13,22 @@ mqttClient.on('connect', function () {
     console.log("connected to server");
     mqttClient.subscribe('#');
 });
+var messages = [];
 mqttClient.on('message', function (topic, message) {
     const rule = rules[topic];
     if(rule && rule['value'] == message){
         console.log("Receive: %s %s", topic, message);
         rule['messages'].forEach(element => {
-            mqttClient.publish(element['topic'], element['payload']);
+            messages.push([element['topic'], payloadelement['payload']]);
+            
             console.log("Publish: %s %s", element['topic'], element['payload']);
         });
       
     }
 });
+setInterval(()=>{
+    if(messages.length()>0){
+        var params = messages.pop();
+        mqttClient.publish(params);
+    }
+}, 1000);
